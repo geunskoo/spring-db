11e3073a857f0063accb430ee95128353bdb0566
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-07-27 08:30:52 +0900

    init: 스프링 DB 솔루션 파일 생성

--- spring-db/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
+++ spring-db/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.geunskoo.springdb;
+package hello.itemservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDbApplicationTests {
+class ItemServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
